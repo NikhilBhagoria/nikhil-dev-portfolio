fbf3dc62b1b265934f6c8acd27c4fc4bfc0846d6
Nikhil Bhagoria <nikhilkumar2450@gmail.com>
2026-07-22 23:29:49 +0530

    fix: prevent link rendering in Button component when disabled

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -55,7 +55,7 @@ export function Button({
     </>
   );
 
-  if (href) {
+  if (href && !disabled) {
     const computedTitle = props.title || (typeof children === "string" ? children : undefined);
     return (
       <Link href={href} className={classes} title={computedTitle} prefetch={false} {...props}>
